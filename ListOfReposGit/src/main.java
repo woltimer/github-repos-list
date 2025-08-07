97e0d5131aaedf624c99c29d69d768c35f95762e
woltimer <141652450+woltimer@users.noreply.github.com>
2025-08-07 22:03:00 +0200

    Update and rename main.java to Main.java

--- ListOfReposGit/src/main.java
+++
@@ -1,8 +0,0 @@
-import java.io.IOException;
-
-public class main {
-	public static void main(String[] args) throws IOException, InterruptedException {
-		new ListOfRepos().getRepos("torvalds");
-	}
-}
-
